b00048c64f4f9735def53999ecfc0c0cd08a9f66
Deanna Heffelfinger <dheffelf@umail.iu.edu>
2017-06-07 16:16:00 -0400

    dino object with id

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const app = {
     init(formSelector){
+        this.max=0
         document
         .querySelector(formSelector)
-        .addEventListener('submit', this.addDino )        
+        .addEventListener('submit', this.addDino.bind())        
     },
 addDino(ev){
     ev.preventDefault()
-    const dinoName = ev.target.dinoName.value
-    console.log(dinoName) 
+    //const dinoName = ev.target.dinoName.value
+    //object 
+    const dino = {
+        //increasing with every dino added
+        id: this.max + 1,
+        name: ev.target.dinoName.value, 
+    }
+    
+    console.log(dino.name, dino.id) 
+    this.max ++ 
 
     },
 
