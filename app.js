5e15455cd28ca265209ed15df9ee16551e71a183
Deanna Heffelfinger <dheffelf@umail.iu.edu>
2017-06-08 15:59:25 -0400

    remvoing dino

--- app.js
+++ app.js
@@ -1,65 +1,61 @@
 const app = {
-    init(selectors){
-        this.dinos = []
-        this.max=0
-        this.list = document
-            .querySelector(selectors.listSelector)
-        this.template = document
-             .querySelector(selectors.templateSelector)
-        document
-             .querySelector(selectors.formSelector)
-             .addEventListener('submit', this.addDino.bind(this))   
+  init(selectors) {
+    this.dinos = []
+    this.max = 0
+    this.list = document
+      .querySelector(selectors.listSelector)
+    this.template = document
+      .querySelector(selectors.templateSelector)
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', this.addDino.bind(this))
+  },
 
-      
-
-    },
-addDino(ev){
+  addDino(ev) {
     ev.preventDefault()
-    //const dinoName = ev.target.dinoName.value
-    //object 
-    
-    
+
     const dino = {
-        //increasing with every dino added
-        id: this.max + 1,
-        name: ev.target.dinoName.value, 
+      id: this.max + 1,
+      name: ev.target.dinoName.value,
     }
+
     const listItem = this.renderListItem(dino)
-    //adding input to begining of list 
-    this.list.insertBefore(listItem,this.list.firstChild)
+    this.list.insertBefore(listItem, this.list.firstChild)
 
-
-    //TODO: add dino to this.dinos array
-    //makes array of objects 
-    //adding to the end to the array "push"
-    //this.dinos.push(dino)
     this.dinos.unshift(dino)
 
-
-
-
-    //console.log(dino.name, dino.id) 
-    this.max ++
-    //clearing out the input so do not have to backspace 
+    ++ this.max
     ev.target.reset()
+  },
 
- }, 
-    renderListItem(dino){
-        const item = this.template.cloneNode()
-        debugger
-        item.dataset.id = dino.id
-        item
-            .querySelector('.dino-name')
-        //const item = document.createElement('li')
-            .textContent = dino.name
-        
-        
-        return item 
+  renderListItem(dino) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = dino.id
+
+    item
+      .querySelector('.dino-name')
+      .textContent = dino.name
+
+    item
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeDino.dino(this))
+
+    return item
+  },
+
+  removeDino(ev) {
+    const listItem = ev.target.closest('.dino')
+    listItem.remove()
+    for(let i=0; i< this.dinos.lenght; i++){
+        this.dinos[i].id
+    }
+    //this.dinos.splice(?, 1)
+  },
 }
-}
-//passing object
+
 app.init({
-    formSelector: '#dino-form',
-    listSelector: '#dino-list',
-    templateSelector: '.dino.template',
+  formSelector: '#dino-form',
+  listSelector: '#dino-list',
+  templateSelector: '.dino.template',
 })
