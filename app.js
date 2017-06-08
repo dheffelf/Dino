e71420244c3ebd7db37eba6a18dcb7efe342d1a3
Deanna Heffelfinger <dheffelf@umail.iu.edu>
2017-06-08 14:30:21 -0400

    add auto

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const app = {
     init(selectors){
+        //this.dinos()
         this.max=0
        this.list = document.querySelector(selectors.listSelector)
         document
         .querySelector(selectors.formSelector)
-        .addEventListener('submit', this.addDino.bind(this))        
+        .addEventListener('submit', this.addDino.bind(this))   
+
+        //making when refreshed will not have to click in box
+        
+
     },
 addDino(ev){
     ev.preventDefault()
@@ -20,8 +25,13 @@ addDino(ev){
     const listItem = this.renderListItem(dino)
     this.list.appendChild(listItem)
 
+
+    //TODO: add dino to this.dinos array
+
     //console.log(dino.name, dino.id) 
     this.max ++ 
+    //clearing out the input so do not have to backspace 
+    ev.target.reset()
 
     }, 
     renderListItem(dino){
